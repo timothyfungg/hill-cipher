269b36bc00c416ec7925bff93b60a19e16368de0
timothyfungg <timothyfung22@gmail.com>
2025-12-28 17:25:03 -0500

    Update HillCipher.py
    Created encrypt method

--- HillCipher.py
+++ HillCipher.py
@@ -1,18 +1,35 @@
 class HillCipher:
-    def __init__(self, key): # key in the form of [[a, c]^T, [b, d]^T]
+    def __init__(self, key): # key in the form of row matrix [[a, b], [c, d]]
         self.key = key
     
-    def charToInt(char):
+    def charToInt(cls, char):
         return (ord(char.capitalize()) - 65)
     
-    def groupInts(ints): # integer list
+    def groupInts(cls, ints): # integer list
         # adds dummy integer if odd number of letters
         if((len(ints) % 2) != 0):
             ints.append(0)
 
-        output = []
+        pairs = []
         for i in range(0, len(ints) - 2, 2):
-            output += [ints[i], ints[i + 1]]
+            pairs.append([ints[i], ints[i + 1]])
+        
+        return pairs # pairs in the form of [[a_1, a_2], [a_3, a_4], . . .], where each list is a column vector
     
     def encrypt(self, text):
+        ints = []
+        for i in range(0, len(text)):
+            ints.append(HillCipher.charToInt(text[i]))
         
+        pairs = HillCipher.groupInts(ints)
+        encryptedPairs = []
+        for i in range(0, len(pairs)):
+            pairs += []
+
+        # matrix multiply key * pairs[i]
+        for i in range(0, len(pairs)):
+            for j in range(0, 2):
+                encryptedPairs[i].append((self.key[0][0] * pairs[i][0]) + (self.key[0][1] * pairs[i][1]))
+                encryptedPairs[i].append((self.key[1][0] * pairs[i][0]) + (self.key[1][1] * pairs[i][1]))
+
+        return encryptedPairs

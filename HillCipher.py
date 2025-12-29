dbbc36eec75dfb6019827e08d3dced35754c603e
timothyfungg <timothyfung22@gmail.com>
2025-12-28 21:15:10 -0500

    Update HillCipher.py
    Created invertKey method

--- HillCipher.py
+++ HillCipher.py
@@ -1,6 +1,7 @@
 class HillCipher:
     def __init__(self, key): # key in the form of row matrix [[a, b], [c, d]]
         self.key = key
+        self.invertedKey = self.invertKey()
     
     def charToInt(cls, char):
         return (ord(char.capitalize()) - 65)
@@ -16,6 +17,34 @@ class HillCipher:
         
         return pairs # pairs in the form of [[a_1, a_2], [a_3, a_4], . . .], where each list is a column vector
     
+    def invertKey(self):
+        det = self.key[0][0] * self.key[1][1] - self.key[0][1] * self.key[1][0]
+        mod26 = { # inverse mod 26 for all possible 2x2 determinants
+            1: 1,
+            3: 9,
+            5: 21,
+            7: 15,
+            9: 3,
+            11: 19,
+            15: 7,
+            17: 23,
+            19: 11,
+            21: 5,
+            23: 17,
+            25: 25
+        }
+        try:
+            detMod26Inverse = mod26[det]
+        except KeyError:
+            raise Exception("Invalid key (no inverse)")
+        
+        # create 2x2 adjoint matrix
+        adjKey = [[self.key[1][1], - self.key[0][1]], [- self.key[1][0], self.key[0][0]]]
+        # multiply adjoint by inverse of determinant
+        invertedKey = [[adjKey[1][1] * detMod26Inverse, - adjKey[0][1]] * detMod26Inverse, [- adjKey[1][0] * detMod26Inverse, adjKey[0][0]] * detMod26Inverse]
+
+        return invertedKey
+    
     def encrypt(self, text):
         ints = []
         for i in range(0, len(text)):
@@ -29,7 +58,7 @@ class HillCipher:
         # matrix multiply key * pairs[i]
         for i in range(0, len(pairs)):
             for j in range(0, 2):
-                encryptedPairs[i].append((self.key[0][0] * pairs[i][0]) + (self.key[0][1] * pairs[i][1]))
-                encryptedPairs[i].append((self.key[1][0] * pairs[i][0]) + (self.key[1][1] * pairs[i][1]))
+                encryptedPairs[i].append(((self.key[0][0] * pairs[i][0]) + (self.key[0][1] * pairs[i][1])) % 26)
+                encryptedPairs[i].append(((self.key[1][0] * pairs[i][0]) + (self.key[1][1] * pairs[i][1])) % 26)
 
         return encryptedPairs
